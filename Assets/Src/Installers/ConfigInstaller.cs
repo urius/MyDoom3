e6a2cf51444321026009e703693a1575ca7d37c2
is-up <isup89@gmail.com>
2020-05-28 00:06:22 +0300

    Part of work

--- Assets/Src/Installers/ConfigInstaller.cs
+++ Assets/Src/Installers/ConfigInstaller.cs
@@ -5,16 +5,19 @@ using Zenject;
 public class ConfigInstaller : ScriptableObjectInstaller<ConfigInstaller>
 {
     [SerializeField]
+    private LocalizationProvider _localizationProvider;
+    [SerializeField]
     private ShipsConfigProvider _shipsConfigProvider;
     [SerializeField]
     private WeaponsConfigProvider _bulletsConfigProvider;
     [SerializeField]
-    private LocalizationProvider _localizationProvider;
+    private ShieldsConfigProvider _shieldsConfigProvider;
 
     public override void InstallBindings()
     {
+        Container.BindInstance(_localizationProvider);
         Container.BindInstance(_shipsConfigProvider);
         Container.BindInstance(_bulletsConfigProvider);
-        Container.BindInstance(_localizationProvider);
+        Container.BindInstance(_shieldsConfigProvider);
     }
 }
